e1d4529a73eeccff672969d1e7a3fedbe1653e5b
RavinduCW <ravinducw@gmail.com>
2018-09-04 16:45:40 +0530

    SignUp and Login Features

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,6 +50,11 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
+            'username' => 'required|string|min:4|unique:users',
+            'nic' => 'required|string|min:10',
+            'dob' => 'required',
+            'address' => 'required',
+            'contactno' => 'required|min:10|numeric',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
@@ -65,6 +70,11 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'username' => $data['username'],
+            'nic' => $data['nic'],
+            'dob' => $data['dob'],
+            'address' => $data['address'],
+            'contactno' => $data['contactno'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
