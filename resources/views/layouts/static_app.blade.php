b8a0cb1a1ff1266ed39bf4daf5367f7295995f88
Ashera Silva <asherasilva100@gmail.com>
2018-09-08 17:35:39 +0530

    Static class packages basic layout added

---
+++ resources/views/layouts/static_app.blade.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>N Studio Zumba</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="{{ URL::asset('favicon/logo.png') }}">
+    <link rel="apple-touch-icon" href="{{ URL::asset('favicon/apple-icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ URL::asset('favicon/apple-icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ URL::asset('favicon/apple-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ URL::asset('favicon/apple-icon-144x144.png') }}">
+
+    <!--All Css Here-->
+
+    <!--Bootstrap Css-->
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+    <!--revolution slider-->
+    <link rel="stylesheet" href="{{ URL::asset('css/settings.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/navigation.css') }}">
+    <!--Font-Awesome Css-->
+    <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.css') }}">
+    <!--flat-icon-->
+    <link rel="stylesheet" href="{{ URL::asset('css/flaticon.css') }}">
+    <!--Owl-Carousel Css-->
+    <link rel="stylesheet" href="{{ URL::asset('css/owl.carousel.css') }}">
+    <!--Animate Css-->
+    <link rel="stylesheet" href="{{ URL::asset('css/animate.css') }}">
+    <!--Animate Css-->
+    <link rel="stylesheet" href="{{ URL::asset('css/magnific-popup.css') }}">
+    <!--Jquery Ui Css-->
+    <link rel="stylesheet" href="{{ URL::asset('css/jquery-ui.min.css') }}">
+    <!--Style Css-->
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+    <!--Responsive Css-->
+    <link rel="stylesheet" href="{{ URL::asset('css/responsive.css') }}">
+    <!--Modernizr Css-->
+    <script src="{{ URL::asset('js/modernizr-2.8.3.min.js') }}"></script>
+
+
+    {{--<!--[if lt IE 9]>--}}
+    {{--<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>--}}
+    {{--<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>--}}
+    {{--<![endif]-->--}}
+
+</head>
+<body>
+
+    <!--main-container-->
+    <div class="main-container">
+        @yield('content')
+
+        <!--Footer start-->
+            @include('static_pages.footer');
+        <!--Footer end-->
+
+    </div>
+    <!--main-container-->
+
+
+    <!--Search Popup-->
+    <div id="search-popup" class="search-popup">
+        <div class="close-search theme-btn"><span class="fa fa-close"></span></div>
+        <div class="popup-inner">
+
+            <div class="search-form">
+                <form method="post" action="index">
+                    <div class="form-group">
+                        <fieldset>
+                            <input type="search" class="form-control" name="search-input" value="" placeholder="Search Here" required>
+                            <input type="submit" value="Search" class="theme-btn">
+                        </fieldset>
+                    </div>
+                </form>
+
+                <br>
+                <h3>Recent Search Keywords</h3>
+            </div>
+        </div>
+    </div>
+    <!--End Search Popup-->
+
+
+    <!--All Js Here-->
+    <!-- jquery latest version -->
+    <script data-cfasync="false" src="{{ URL::asset('js/email-decode.min.js') }}"></script><script src="{{ URL::asset('js/jquery-3.2.1.min.js') }}"></script>
+    <!--Migrate Js-->
+    <script src="{{ URL::asset('js/jquery-migrate.js') }}"></script>
+    <!--Popper Js-->
+    <script src="{{ URL::asset('js/popper-1.12.3.min.js') }}"></script>
+    <!--Bootstrap Js-->
+    <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+    <!--Owl-Carousel Js-->
+    <script src="{{ URL::asset('js/owl.carousel.min.js') }}"></script>
+    <!--counter Js-->
+    <script src="{{ URL::asset('js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ URL::asset('js/waypoints-jquery.js') }}"></script>
+    <!--Isotop Js-->
+    <script src="{{ URL::asset('js/isotope.pkgd.min.js') }}"></script>
+
+    <!-- revolution slider js files start -->
+    <script src="{{ URL::asset('js/jquery.themepunch.tools.min.js') }}"></script>
+    <script src="{{ URL::asset('js/revolution.min.js') }}"></script>
+
+    <script src="{{ URL::asset('js/revolution.extension.actions.min.js') }}"></script>
+    <script src="{{ URL::asset('js/revolution.extension.carousel.min.js') }}"></script>
+    <script src="{{ URL::asset('js/revolution.extension.kenburn.min.js') }}"></script>
+    <script src="{{ URL::asset('js/revolution.extension.layeranimation.min.js') }}"></script>
+    <script src="{{ URL::asset('js/revolution.extension.migration.min.js') }}"></script>
+    <script src="{{ URL::asset('js/revolution.extension.navigation.min.js') }}"></script>
+    <script src="{{ URL::asset('js/revolution.extension.parallax.min.js') }}"></script>
+    <script src="{{ URL::asset('js/revolution.extension.slideanims.min.js') }}"></script>
+    <script src="{{ URL::asset('js/revolution.extension.video.min.js') }}"></script>
+    <!-- revolution slider js files end -->
+
+
+    @yield('js_styling')
+
+
+    <!--magnific popup Js-->
+    <script src="{{ URL::asset('js/jquery.magnific-popup.min.js') }}"></script>
+    <!--scrollUp js-->
+    <script src="{{ URL::asset('js/jquery.scrollUp.js') }}"></script>
+    <!--Jquery Ui Js-->
+    <script src="{{ URL::asset('js/jquery-ui.min.js') }}"></script>
+    <!--Wow Js-->
+    <script src="{{ URL::asset('js/wow.min.js') }}"></script>
+
+    <!-- template main js file -->
+    <script src="{{ URL::asset('js/main.js') }}"></script>
+</body>
+</html>
--- resources/views/static_pages/class_packages.blade.php
+++ resources/views/static_pages/class_packages.blade.php
@@ -1,1 +1,208 @@
-<h1>Class Packages</h1>
+@extends('layouts.static_app')
+
+
+@section('content')
+
+    <!-- /.header start -->
+    <header class="header fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index"><img height="80px" width="80px" src={{ URL::asset('images/logo_nav.png') }}  alt="N_Studio_Zumba_Logo"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link " href="/N_Studio_Zumba/public/index">
+                                home<span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="/N_Studio_Zumba/public/index/about">
+                                about<span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="/N_Studio_Zumba/public/index/gallery">
+                                gallery<span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link " href="/N_Studio_Zumba/public/index/class_packages">
+                                classes<span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="/N_Studio_Zumba/public/index/schedule">
+                                schedule<span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="/N_Studio_Zumba/public/index/testimonials">
+                                testimonials<span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/N_Studio_Zumba/public/index/contact">contact</a>
+                        </li>
+                        <li class="nav-item d-none d-lg-inline">
+                            <div class="icon-menu">
+                                <ul>
+                                    <li><a href="#" class="search-btn search-box-btn"><i class="fa fa-search"></i></a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">register</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!--header end-->
+
+
+
+    <!-- page title & breadcrumbs start -->
+    <div class="pricing-plan-bg page-head parallax overlay">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center">
+                        <h3>class packages</h3>
+                    </div>
+                </div>
+                <!-- /.colour-service-1-->
+                <div class="col-md-12">
+                    <ol class="breadcrumb">
+                        <li><a href="/N_Studio_Zumba/public/index">home</a></li>
+                        <li>।</li>
+                        <li>Classes</li>
+                    </ol>
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.page-header -->
+    <!-- page title & breadcrumbs end -->
+
+    <!--pricing area start-->
+    <div class="pricing-area text-center pad90">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center">
+                        <div class="title-bar full-width mb20">
+                            <img src="{{ URL::asset('images/logo/ttl-bar.png') }}"  alt="title-img">
+                        </div>
+                        <h3>packages and pricing plans</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="price-box">
+                        <div class="price-empty">
+                        </div>
+                        <div class="price-quantity">
+                            <div class="qnty-box">
+                                <div class="box-element">
+                                    <h5>£85.00</h5>
+                                    <p>Quarterly</p>
+                                </div>
+                            </div>
+                            <div class="price-dtl">
+                                <ul>
+                                    <li class="first-child">3 Days of the week</li>
+                                    <li>professional trainer</li>
+                                    <li>Bodybuilding</li>
+                                    <li>Free Hand</li>
+                                    <li>Fitness & Boxing</li>
+                                </ul>
+                                <div class="price-btn bttn">
+                                    <button type="submit" class="btn btn-primary">Buy now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.col -->
+                <div class="col-md-4">
+                    <div class="price-box active">
+                        <div class="price-empty">
+                        </div>
+                        <div class="price-quantity">
+                            <div class="qnty-box">
+                                <div class="box-element">
+                                    <h5>£130.00</h5>
+                                    <p>half yearly</p>
+                                </div>
+                            </div>
+                            <div class="price-dtl">
+                                <ul>
+                                    <li class="first-child">3 Days of the week</li>
+                                    <li>professional trainer</li>
+                                    <li>Bodybuilding</li>
+                                    <li>Free Hand</li>
+                                    <li>Fitness & Boxing</li>
+                                </ul>
+                                <div class="price-btn bttn">
+                                    <button type="submit" class="btn btn-primary">Buy now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.col -->
+                <div class="col-md-4">
+                    <div class="price-box">
+                        <div class="price-empty">
+                        </div>
+                        <div class="price-quantity">
+                            <div class="qnty-box">
+                                <div class="box-element">
+                                    <h5>£85.00</h5>
+                                    <p>£210.00</p>
+                                </div>
+                            </div>
+                            <div class="price-dtl">
+                                <ul>
+                                    <li class="first-child">3 Days of the week</li>
+                                    <li>professional trainer</li>
+                                    <li>Bodybuilding</li>
+                                    <li>Free Hand</li>
+                                    <li>Fitness & Boxing</li>
+                                </ul>
+                                <div class="price-btn bttn">
+                                    <button type="submit" class="btn btn-primary">Buy now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </div>
+    <!--pricing area end-->
+
+
+@endsection
+
+
+
+
+
+
