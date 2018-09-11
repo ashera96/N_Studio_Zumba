4a08d3624738cde5e3af3f0a3f9c43b5ff28be93
Ashera Silva <asherasilva100@gmail.com>
2018-09-11 15:42:23 +0530

    Redirections of the navbar eddited

--- resources/views/static_pages/navbar.blade.php
+++ resources/views/static_pages/navbar.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container">
             <div class="navbar-header">
-                <a class="navbar-brand" href="/N_Studio_Zumba/public/index"><img height="80px" width="80px" src={{ URL::asset('images/logo_nav.png') }}  alt="N_Studio_Zumba_Logo"></a>
+                <a class="navbar-brand" href="/index"><img height="80px" width="80px" src={{ URL::asset('images/logo_nav.png') }}  alt="N_Studio_Zumba_Logo"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -10,37 +10,37 @@
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link " href="/N_Studio_Zumba/public/index">
+                        <a class="nav-link " href="/index">
                             home<span class="sr-only">(current)</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link " href="/N_Studio_Zumba/public/index/about">
+                        <a class="nav-link " href="/index/about">
                             about<span class="sr-only">(current)</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link " href="/N_Studio_Zumba/public/index/gallery">
+                        <a class="nav-link " href="/index/gallery">
                             gallery<span class="sr-only">(current)</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link " href="/N_Studio_Zumba/public/index/class_packages">
+                        <a class="nav-link " href="/index/class_packages">
                             classes<span class="sr-only">(current)</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link " href="/N_Studio_Zumba/public/index/schedule">
+                        <a class="nav-link " href="/index/schedule">
                             schedule<span class="sr-only">(current)</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link " href="/N_Studio_Zumba/public/index/testimonials">
+                        <a class="nav-link " href="/index/testimonials">
                             testimonials<span class="sr-only">(current)</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/N_Studio_Zumba/public/index/contact">contact</a>
+                        <a class="nav-link" href="/index/contact">contact</a>
                     </li>
                     <li class="nav-item d-none d-lg-inline">
                         <div class="icon-menu">
@@ -50,10 +50,10 @@
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">login</a>
+                        <a class="nav-link" href="{{ route('login') }}">login</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">register</a>
+                        <a class="nav-link" href="{{ route('register') }}">register</a>
                     </li>
                 </ul>
             </div>
