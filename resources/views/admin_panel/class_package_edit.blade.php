3fa6366895ef9fc05a69576db478f51e6eea0eb9
RavinduW <raviduchinthaka3@gmail.com>
2018-10-02 22:45:39 +0530

    class Packages form validation
    print a error message when a validation error has occured in class package add and edit form

--- resources/views/admin_panel/class_package_edit.blade.php
+++ resources/views/admin_panel/class_package_edit.blade.php
@@ -26,12 +26,22 @@
                                 <div class="form-group">
                                     <h5 style="color: #343a40">Package Name</h5>
                                     <input type="text" name="name" value="{{$package->name}}" class="form-control ">
+                                    @if ($errors->has('name'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                    @endif
                                 </div>
                             </div>
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <h5 style="color: #343a40">Package Price</h5>
                                     <input type="text" name="price" value="{{$package->price}}" class="form-control">
+                                    @if ($errors->has('price'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('price') }}</strong>
+                                    </span>
+                                    @endif
                                 </div>
                             </div>
                         </div>
@@ -40,6 +50,11 @@
                                 <div class="form-group">
                                     <h5 style="color: #343a40">Service</h5>
                                     <input type="text" name="services" value="{{$package->services}}" class="form-control">
+                                    @if ($errors->has('services'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('services') }}</strong>
+                                    </span>
+                                    @endif
                                 </div>
                             </div>
                         </div>
@@ -48,6 +63,11 @@
                                 <div class="form-group">
                                     <h5 style="color: #343a40">Classes Per Month</h5>
                                     <input type="number" name="classes_to_cover" value="{{$package->classes_to_cover}}" class="form-control">
+                                    @if ($errors->has('classes_to_cover'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('classes_to_cover') }}</strong>
+                                    </span>
+                                    @endif
                                 </div>
                             </div>
                         </div>
