cd8ef11311ba96252630c126af6326a3fd7fefc8
Pirakavi <sspirakavi@gmail.com>
2018-11-04 22:59:43 +0530

    UI changes
    add UserWeighController, changes in style.css

--- resources/views/admin_panel/dashboard.blade.php
+++ resources/views/admin_panel/dashboard.blade.php
@@ -88,7 +88,7 @@
                         <a href="/admin/customers" class="list-group-item side-bar"><i class="fa fa-users fa-lg mr-1"></i> Customers</a>
                         <a href="users.html" class="list-group-item side-bar"><i class="fa fa-bell fa-lg mr-1"></i> Notifications</a>
                         <a href="/admin/customers" class="list-group-item side-bar"><i class="fa fa-dollar fa-lg mr-1"></i> Payments</a>
-                        <a href="users.html" class="list-group-item side-bar"><i class="fa fa-file fa-lg mr-1"></i> Reports</a>
+                        <a href="/admin/reports" class="list-group-item side-bar"><i class="fa fa-file fa-lg mr-1"></i> Reports</a>
                     </div>
                 </div>
                 <!--Sidebar-area end-->
--- resources/views/admin_panel/user_index.blade.php
+++ resources/views/admin_panel/user_index.blade.php
@@ -1,57 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Admin area | Dashboard</title>
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="{{ URL::asset('favicon/logo144x144.png') }}">
-    <!-- Bootstrap core CSS -->
-    <link href="{{ URL::asset('css/dashboard-css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ URL::asset('css/dashboard-css/style.css') }}" rel="stylesheet">
-    <!-- Materil icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+@extends('layouts.admin_app');
 
-    <script src="https://cdn.ckeditor.com/4.10.1/standard/ckeditor.js"></script>
-</head>
+@section('content')
 
-<body>
-<nav class="navbar navbar-default ">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">N Studio Zumba</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="/index">Home</a></li>
-                <li><a href="/index/about">About</a></li>
-                <li><a href="/index/gallery">Gallery</a></li>
-                <li><a href="/admin/dashboard/class_packages">Classes</a></li>
-                <li><a href="/admin/dashboard/schedule">Schedules</a></li>
-                <li><a href="/index/testimonials">Testimonials</a></li>
-                <li><a href="/index/contact">Contact</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Welcome, Admin</a></li>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+    <!-- /.header start -->
+    <header class="header fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/home"><img src="{{ URL::asset('images/logo/nav_logo.png') }}"  alt="N_Studio_Zumba_Logo"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link " href="/index">
+                                home<span class="sr-only">(current)</span>
+                            </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            <a class="nav-link " href="/index/about">
+                                about<span class="sr-only">(current)</span>
+                            </a>
                         </li>
-                    @else
+                        <li class="nav-item">
+                            <a class="nav-link " href="/index/gallery">
+                                gallery<span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="/index/class_packages">
+                                classes<span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="/index/schedule">
+                                schedule<span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="/index/testimonials">
+                                testimonials<span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/index/contact">contact</a>
+                        </li>
+
+                        {{--User name and logout button start--}}
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->username }} <span class="caret"></span>
@@ -60,7 +57,7 @@
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                                 document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
 
@@ -69,183 +66,91 @@
                                 </form>
                             </div>
                         </li>
-                    @endguest
-                </ul>
-            </ul>
-        </div><!--/.nav-collapse -->
-    </div>
-</nav>
+                        {{--User name and logout button end--}}
 
-<header id="header">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10">
-                <h1><i class="material-icons prefix">settings</i> Dashboard <small>Manage your site</small></h1>
-            </div>
-
-
-
-
-        </div>
-    </div>
-</header>
-
-<section id="breadcrumb">
-    <div class="container">
-        <ol class="breadcrumb">
-            <li class="active">Customers</li>
-        </ol>
-    </div>
-</section>
-
-<section id="main">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <div class="list-group">
-                    <a href="/admin/dashboard" class="list-group-item  ">
-
-                        <i class="material-icons prefix ">settings</i> Dashboard
-                    </a>
-
-                    <a href="/admin/receptionist" class="list-group-item  "><i class="material-icons prefix">perm_identity</i> Receptionist<span class="badge">1</span></a>
-                    <a href="/admin/customers" class="list-group-item active main-color-bg"><i class="material-icons prefix">person</i> Customers<span class="badge">66</span></a>
-                    <a href="users.html" class="list-group-item"><i class="material-icons prefix">notifications</i> Notifications<span class="badge">5</span></a>
-                    <a href="posts.html" class="list-group-item"><i class="material-icons prefix">attach_money</i> Payments<span class="badge">56</span></a>
-                    <a href="/admin/reports" class="list-group-item"><i class="material-icons prefix">file_copy</i> Reports<span class="badge">10</span></a>
-
-                </div>
-                <div class="well">
-                    <h4>Disk Space Used</h4>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-                            60%
-                        </div>
-                    </div>
-                    <h4>Bandwidth Used</h4>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
-                            40%
-                        </div>
-                    </div>
+                    </ul>
                 </div>
             </div>
-            <div class="col-md-9">
-                <!-- Website Overview -->
-                <div class="panel panel-default">
-                    <div class="panel-heading main-color-bg">
-                        <h3 class="panel-title">Customers Overview</h3>
+        </nav>
+    </header>
+    <!--header end-->
+
+
+    <!--Admin dashboard-area start-->
+    <div class="about-area pad90">
+        <div class="container-fluid">
+            <div class="row">
+                <!--Sidebar-area start-->
+                <div class="col-lg-2 col-md-3">
+                    <div class="list-group shadow-sm">
+                        <a href="/admin/dashboard" class="list-group-item side-bar"><i class="fa fa-cog fa-lg mr-1"></i> Dashboard</a>
+                        <a href="/admin/receptionist" class="list-group-item side-bar"><i class="fa fa-user fa-lg mr-1"></i> Receptionist</a>
+                        <a href="/admin/customers" class="list-group-item active side-bar"><i class="fa fa-users fa-lg mr-1"></i> Customers</a>
+                        <a href="users.html" class="list-group-item side-bar"><i class="fa fa-bell fa-lg mr-1"></i> Notifications</a>
+                        <a href="/admin.html" class="list-group-item side-bar"><i class="fa fa-dollar fa-lg mr-1"></i> Payments</a>
+                        <a href="/admin/reports" class="list-group-item side-bar"><i class="fa fa-file fa-lg mr-1"></i> Reports</a>
+                    </div>
+                </div>
+                <!--Sidebar-area end-->
+
+                <div class="col-lg-10 col-md-9 pad30">
+                    <!-- Website Overview -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading main-color-bg">
+                            <h3 class="panel-title">Customers Overview</h3><br>
+
+                        </div>
 
                     </div>
-
-                </div>
-                <!-- Latest Users -->
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <table class="table thread-dark" width="80%" height="50%" >
-                            <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Username</th>
-                                <th>Email</th>
-                                <th></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($users as $user)
+                    <!-- Latest Users -->
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <table class="table thread-dark" width="80%"  >
+                                <thead>
                                 <tr>
-                                    <td>{{ $user->name }}</td>
-                                    <td>{{ $user->username }}</td>
-                                    <td>{{ $user->email }}</td>
-                                    <td>
-                                        <div class="row">
-                                            <div class="col">
-                                                <a href="{{url('admin/customers/'.$user->id.'/edit')}}"><button class="editbtn" >EDIT</button></a>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <form method="POST" {{--action="{{route('user.destroy',$user->id)}}"--}}>
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="delbtn">DELETE</button>
-                                        </form>
-                                    </td>
+                                    <th>Name</th>
+                                    <th>Username</th>
+                                    <th>Email</th>
+                                    <th></th>
                                 </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                @foreach($users as $user)
+                                    <tr>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->username }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>
+                                            <div class="row">
+                                                <div class="col">
+                                                    <a href="{{url('admin/customers/'.$user->id.'/edit')}}"><button class="editbtn" >EDIT</button></a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <form method="POST" {{--action="{{route('user.destroy',$user->id)}}"--}}>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="delbtn">DELETE</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
 
+                        </div>
                     </div>
                 </div>
+                <!-- /.col -->
             </div>
+            <!-- /.row -->
         </div>
     </div>
-</section>
 
-<footer id="footer">
-    <p>Copyright © <span>N Studio Zumba.</span> All Rights Reserved</p>
-</footer>
+@endsection
 
-<!-- Modals -->
-
-<!-- Add Page -->
-<div class="modal fade" id="addPage" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <form>
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Add Page</h4>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label>Page Title</label>
-                        <input type="text" class="form-control" placeholder="Page Title">
-                    </div>
-                    <div class="form-group">
-                        <label>Page Body</label>
-                        <textarea name="editor1" class="form-control" placeholder="Page Body"></textarea>
-                    </div>
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox"> Published
-                        </label>
-                    </div>
-                    <div class="form-group">
-                        <label>Meta Tags</label>
-                        <input type="text" class="form-control" placeholder="Add some Tags....">
-                    </div>
-                    <div class="form-group">
-                        <label>Meta Description</label>
-                        <input type="text" class="form-control" placeholder="Add some Description....">
-                    </div>
-
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Save changes</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-
-
-<script>
-    CKEDITOR.replace( 'editor1' );
-</script>
-
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="{{ URL::asset('js/dashboard-js/bootstrap.min.js') }}"></script>
-
-
-</body>
-</html>
-
+@section('js_styling')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="{{ URL::asset('js/dashboard-js/bootstrap.min.js') }}"></script>
+@endsection
