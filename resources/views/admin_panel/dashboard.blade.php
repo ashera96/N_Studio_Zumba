1bc1f2463b9aeb5383f8ec96b6ccca00d7a3d6c6
Ashera Silva <asherasilva100@gmail.com>
2018-11-07 15:21:52 +0530

    Added the additional tabs to the admin dashboard

--- resources/views/admin_panel/dashboard.blade.php
+++ resources/views/admin_panel/dashboard.blade.php
@@ -89,6 +89,9 @@
                         <a href="users.html" class="list-group-item side-bar"><i class="fa fa-bell fa-lg mr-1"></i> Notifications</a>
                         <a href="/admin/customers" class="list-group-item side-bar"><i class="fa fa-dollar fa-lg mr-1"></i> Payments</a>
                         <a href="/admin/reports" class="list-group-item side-bar"><i class="fa fa-file fa-lg mr-1"></i> Reports</a>
+                        <a href="/admin/dashboard" class="list-group-item side-bar"><i class="fa fa-image fa-lg mr-1"></i> Gallery</a>
+                        <a href="/admin/dashboard" class="list-group-item side-bar"><i class="fa fa-clipboard fa-lg mr-1"></i> Classes</a>
+                        <a href="/admin/dashboard" class="list-group-item side-bar"><i class="fa fa-calendar fa-lg mr-1"></i> Schedules</a>
                     </div>
                 </div>
                 <!--Sidebar-area end-->
