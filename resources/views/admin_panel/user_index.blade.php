0893954227735527f95f30dd03af62934dc6fafc
Keshi <anukeshi96@gmail.com>
2018-11-15 23:22:21 +0530

    dashboard base design included in all admin pages

--- resources/views/admin_panel/user_index.blade.php
+++ resources/views/admin_panel/user_index.blade.php
@@ -14,7 +14,7 @@
                 </div>
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
+                        <li class="nav-item ">
                             <a class="nav-link " href="/index">
                                 home<span class="sr-only">(current)</span>
                             </a>
@@ -83,26 +83,39 @@
                 <!--Sidebar-area start-->
                 <div class="col-lg-2 col-md-3">
                     <div class="list-group shadow-sm">
-                        <a href="/admin/dashboard" class="list-group-item side-bar"><i class="fa fa-cog fa-lg mr-1"></i> Dashboard</a>
+                        <a href="/admin/dashboard" class="list-group-item  side-bar"><i class="fa fa-cog fa-lg mr-1"></i> Dashboard</a>
                         <a href="/admin/receptionist" class="list-group-item side-bar"><i class="fa fa-user fa-lg mr-1"></i> Receptionist</a>
                         <a href="/admin/customers" class="list-group-item active side-bar"><i class="fa fa-users fa-lg mr-1"></i> Customers</a>
-                        <a href="users.html" class="list-group-item side-bar"><i class="fa fa-bell fa-lg mr-1"></i> Notifications</a>
-                        <a href="/admin.html" class="list-group-item side-bar"><i class="fa fa-dollar fa-lg mr-1"></i> Payments</a>
+                        <a href="admin/create_notifications" class="list-group-item side-bar"><i class="fa fa-bell fa-lg mr-1"></i> Notifications</a>
+                        <a href="/admin/dashboard" class="list-group-item side-bar"><i class="fa fa-dollar fa-lg mr-1"></i> Payments</a>
                         <a href="/admin/reports" class="list-group-item side-bar"><i class="fa fa-file fa-lg mr-1"></i> Reports</a>
+                        <a href="/admin/dashboard/admin_gallery" class="list-group-item side-bar"><i class="fa fa-image fa-lg mr-1"></i> Gallery</a>
+                        <a href="/admin/dashboard/class_packages" class="list-group-item side-bar"><i class="fa fa-clipboard fa-lg mr-1"></i> Classes</a>
+                        <a href="/admin/dashboard" class="list-group-item side-bar"><i class="fa fa-calendar fa-lg mr-1"></i> Schedules</a>
                     </div>
                 </div>
                 <!--Sidebar-area end-->
 
                 <div class="col-lg-10 col-md-9 pad30">
-                    <!-- Website Overview -->
-                    <div class="panel panel-default">
-                        <div class="panel-heading main-color-bg">
-                            <h3 class="panel-title">Customers Overview</h3><br>
 
+
+
+
+                    <div class="section-title text-center">
+                        <div class="title-bar full-width mb20">
+                            <img src="{{ URL::asset('images/logo/ttl-bar.png') }}" alt="title-img">
                         </div>
+                        <h3>Customers</h3>
+                        <p>Manage Customers</p>
+                    </div>
+
+
+                    <!-- Customers table -->
+                    <div class="panel panel-default">
+
 
                     </div>
-                    <!-- Latest Users -->
+
                     <div class="panel panel-default">
                         <div class="panel-body">
                             <table class="table thread-dark" width="80%"  >
