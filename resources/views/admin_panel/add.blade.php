ff0310017df6b4bce2dd98c9c828d0bffa62bd7a
Keshi <anukeshi96@gmail.com>
2018-12-28 23:20:55 +0530

    2 sidebar layouts added, recep add page designed

--- resources/views/admin_panel/add.blade.php
+++ resources/views/admin_panel/add.blade.php
@@ -1,107 +1,135 @@
-<html>
-<head>
-    <title>Employees</title>
+@extends('layouts.dashboard_app')
 
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+@section('content')
 
-</head>
-<body>
-
-<a href="/admin/receptionist" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Back</a>
-<div class="container">
-
-    <div class = "card-panel grey lighten-2"><h3 style="text-align: center ;font-family:century gothic">Employees Add</h3></div>
+    @extends('layouts.hori_sidebar');
 
 
-    <div class = "card-panel center">
-        <div class="row">
-
-
-            <form class="col s12" method="POST" action="{{url('admin/dashboard/receptionist')}}">
-
-                @csrf
-                @if(session()->has('success'))
-                    <div class="alert alert-success">
-                        {{ session()->get('success') }}
-                    </div>
-                @endif
-
-                <div class="row">
-                    <div class="input-field col s6">
-                        <i class="material-icons prefix">account_circle</i>
-                      <!--  <input id="name" type="text" class="validate" name="name" value="{{Request::old('name')}}"-->
-                        <input id="name" type="text"  placeholder="Enter name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-
-                        <label for="name">Name</label>
-                        @if($errors->has('name'))
-                            <span class="form-text invalid-feedback" style="color: red">{{$errors->first('name')}}</span>
-                        @endif
-
-                    </div>
-                    <div class="input-field col s6 ">
-                        <i class="material-icons prefix">email</i>
-                        <input id="email" type="email"  placeholder="Enter email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                      <!--  <input id="email" type="tel" class="validate" name="email" value="{{Request::old('email')}}"> -->
-                        <label for="email">Email</label>
-                        @if($errors->has('email'))
-                            <span class="form-text invalid-feedback" style="color: red">{{$errors->first('email')}}</span>
-                        @endif
-
-                    </div>
+    <a href="/admin/receptionist" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Back</a>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="section-title text-center">
+                <div class="title-bar full-width mb20">
+                    <img src="{{ URL::asset('images/logo/ttl-bar.png') }}"  alt="title-img">
                 </div>
-                <div class="row">
-                    <div class="input-field col s6">
-                        <i class="material-icons prefix">contact_mail</i>
-                        <!--<input id="nic" type="text" class="validate" name="nic" value="{{Request::old('nic')}}">-->
-                        <input id="nic" type="text"  placeholder="Enter nic" class="form-control{{ $errors->has('nic') ? ' is-invalid' : '' }}" name="nic" value="{{ old('nic') }}" required autofocus>
-
-                        <label for="nic">NIC</label>
-                        @if($errors->has('nic'))
-                            <span class="form-text invalid-feedback" style="color: red">{{$errors->first('nic')}}</span>
-                        @endif
-                    </div>
-                    <div class="input-field col s6">
-                        <i class="material-icons prefix">cake</i>
-                       <!--<input id="dob" type="date" class="validate" name="dob" value="{{Request::old('dob')}}">-->
-                        <input id="dob" type="date"  placeholder="Enter DOB" class="form-control{{ $errors->has('dob') ? ' is-invalid' : '' }}" name="dob" value="{{ old('dob') }}" required autofocus>
-
-                        <label for="dob">DOB</label>
-                        @if($errors->has('dob'))
-                            <span class="form-text invalid-feedback" style="color: red">{{$errors->first('dob')}}</span>
-                        @endif
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s6">
-                        <i class="material-icons prefix">home</i>
-                       <!-- <input id="address" type="text" class="validate" name="address" value="{{Request::old('address')}}"> -->
-                        <input id="address" type="text"  placeholder="Enter Address" class="form-control{{ $errors->has('address') ? ' is-invalid' : '' }}" name="address" value="{{ old('address') }}" required autofocus>
-
-                        <label for="address">Address</label>
-                        @if($errors->has('address'))
-                            <span class="form-text invalid-feedback" style="color: red">{{$errors->first('address')}}</span>
-                        @endif
-                    </div>
-                    <div class="input-field col s6">
-                        <i class="material-icons prefix">phone</i>
-                        <!--<input id="tpno" type="tel" class="validate" name="tpno" value="{{Request::old('tpno')}}"> -->
-                        <input id="tpno" type="tel"  placeholder="Enter TP no" class="form-control{{ $errors->has('tpno') ? ' is-invalid' : '' }}" name="tpno" value="{{ old('tpno') }}" required autofocus>
-
-                        <label for="tpno">TPno</label>
-                        @if($errors->has('tpno'))
-                            <span class="form-text  invalid-feedback" style="color: red">{{$errors->first('tpno')}}</span>
-                        @endif
-                    </div>
-                </div>
-                <input type="submit" name="submit" class="btn blue right" value="Save">
-
-            </form>
+                <h3>Add Receptionist</h3>
+            </div>
         </div>
     </div>
-</div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
-</body>
-</html>
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <div class="contact-form mt20">
+                <div class="appointment-schedule">
+                    <form class="appointment-form" method="POST" action="{{url('admin/dashboard/receptionist')}}">
+
+                        @csrf
+                        {{--<div class="alert-danger">--}}
+                        {{--@if(count($errors)>0)--}}
+                        {{--@foreach($errors->all() as $error)--}}
+                        {{--<ul>--}}
+                        {{--<li>{{ $error }}</li>--}}
+                        {{--</ul>--}}
+                        {{--@endforeach--}}
+                        {{--@endif--}}
+                        {{--</div>--}}
+
+
+                        <div class="row">
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <h5 style="color: #343a40">Name</h5>
+                                    <input type="text" name="name" placeholder="Ashley Jackson" class="form-control ">
+                                    @if ($errors->has('name'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <h5 style="color: #343a40">Email</h5>
+                                    <input type="email" name="email" placeholder="Ashley@gmail.com" class="form-control ">
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <h5 style="color: #343a40">NIC</h5>
+                                    <input type="text" name="nic" placeholder="XXXXXXXXXXV" class="form-control ">
+                                    @if ($errors->has('nic'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('nic') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <h5 style="color: #343a40">DOB</h5>
+                                    <input type="date" name="dob" placeholder="" class="form-control ">
+                                    @if ($errors->has('dob'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('dob') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <h5 style="color: #343a40">Address</h5>
+                                    <input type="text" name="address" placeholder="60,Galpotha Road,Dehiwala" class="form-control ">
+                                    @if ($errors->has('address'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('address') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <h5 style="color: #343a40">Phone</h5>
+                                    <input type="tel" name="tpno" placeholder="07XXXXXXXX" class="form-control ">
+                                    @if ($errors->has('tpno'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('tpno') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+
+
+
+
+
+                        </div>
+
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="bttn full-width">
+                                    <button name="submit" type="submit" class="btn active full-width btn-primary">Add</button>
+                                </div>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- /.col -->
+    </div>
+@endsection
