dcfa7250a3c4e4012f421b6f941180410ce60145
Ashera Silva <asherasilva100@gmail.com>
2019-01-01 10:46:25 +0530

    Fixed the styling of the Notifications section in the admin panel

--- resources/views/admin_panel/post_update.blade.php
+++ resources/views/admin_panel/post_update.blade.php
@@ -1,121 +1,77 @@
-@extends('layouts.admin_app')
+@extends('layouts.admin_app');
 
-@section('content')
+@section('content');
 
     <!-- /.header start -->
-    <header class="header fixed-top">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="/home"><img src="{{ URL::asset('images/logo/nav_logo.png') }}"  alt="N_Studio_Zumba_Logo"></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link " href="/home">
-                                home<span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/index/about">
-                                about<span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/admin/dashboard/admin_gallery">
-                                gallery<span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/index/class_packages">
-                                classes<span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/index/schedule">
-                                schedule<span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/index/testimonials">
-                                testimonials<span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/index/contact">contact</a>
-                        </li>
-
-                        {{--User name and logout button start--}}
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->username }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                        {{--User name and logout button end--}}
-
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+    @extends('layouts.hori_sidebar');
     <!--header end-->
-    <br><br><br><br><br>
 
-<div class = "container">
-        <form method="POST" action="{{ route('post.edit',$post->id)}}" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
-            <div class="form-group row" style="margin-left: 250px">
-                <div class="col-md-8" >
-                    <input  type="text" id="title" name="title" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}"  placeholder="Post Title" value="{{ $post->title }}"  required><br>
-                    @if ($errors->has('title'))
-                        <span class="invalid-feedback" role="alert">
-                        <strong>{{ $errors->first('title') }}</strong>
-                    </span>
-                    @endif
-                    @if($post->image)
-                        <img src="{{asset('images/posts/' . $post->image)}}" />
-                        <br><br>
-                    @endif
-                    <label style="color: deeppink"> Upload Images: </label> <input style="color: red" type="file" name="upload_images"/>
-                    @if ($errors->has('upload_images'))
-                        <span class="invalid-feedback" role="alert">
-                        <strong>{{ $errors->first('upload_images') }}</strong>
-                    </span>
-                    @endif
-                    <textarea id="post_body" type="text" style="height: 300px" class="form-control{{ $errors->has('post_body') ? ' is-invalid' : '' }}" placeholder="Post Body" name="post_body" required>{{ $post->post_body }}</textarea>
-                    <br>
-                    @if ($errors->has('post_body'))
-                        <span class="invalid-feedback" role="alert">
-                        <strong>{{ $errors->first('post_body') }}</strong>
-                    </span>
-                    @endif
-                </div>
-                <div class="form-group row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary" style="background-color: deeppink;border:none" id="edit">
-                            {{ __('Update') }}
-                        </button>
+    <!--Notification-area start-->
+    <div class="about-area pad90">
+        <div class="container-fluid">
+            <div class="row">
+                <!--Sidebar-area start-->
+                @extends('layouts.vertical_sidebar');
+                <!--Sidebar-area end-->
+
+                <div class="col-lg-10 col-md-9 mainFix col-lg-offset-2 col-md-offset-3">
+
+                    <div class="section-title text-center">
+                        <div class="title-bar full-width mb20">
+                            <img src="{{ URL::asset('images/logo/ttl-bar.png') }}" alt="title-img">
+                        </div>
+                        <h3>Update Post</h3>
                     </div>
+
+                    <!--update post area start-->
+                    <div class="pricing-area text-center pad30 col-md-10 px-4 ml-4">
+                        <div class="container">
+
+                            <form method="POST" action="{{ route('post.edit',$post->id)}}" enctype="multipart/form-data">
+                                @csrf
+                                @method('PUT')
+                                <div class="form-group row" style="margin-left: 250px">
+                                    <div class="col-md-12" >
+                                        <input  type="text" id="title" name="title" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}"  placeholder="Post Title" value="{{ $post->title }}"  required><br>
+                                        @if ($errors->has('title'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('title') }}</strong>
+                                            </span>
+                                        @endif
+                                        @if($post->image)
+                                            <img src="{{asset('images/posts/' . $post->image)}}" />
+                                            <br><br>
+                                        @endif
+                                        <label style="color: deeppink"> Upload Images: </label> <input style="color: red" type="file" name="upload_images"/>
+                                        @if ($errors->has('upload_images'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('upload_images') }}</strong>
+                                            </span>
+                                        @endif
+                                        <textarea id="post_body" type="text" style="height: 300px" class="form-control{{ $errors->has('post_body') ? ' is-invalid' : '' }}" placeholder="Post Body" name="post_body" required>{{ $post->post_body }}</textarea>
+                                        <br>
+                                        @if ($errors->has('post_body'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('post_body') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group row mb-0">
+                                        <div class="col-md-6 offset-md-3" style="margin-left: 250px">
+                                            <button type="submit" class="btn active btn-primary" id="edit">
+                                                {{ __('Update') }}
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                    <!--update post area end-->
+
                 </div>
             </div>
-        </form>
-
-</div>
-
-    @endsection
+        </div>
+    </div>
+@endsection
--- resources/views/admin_panel/show_posts.blade.php
+++ resources/views/admin_panel/show_posts.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.admin_app')
+@extends('layouts.admin_app');
 
-@section('content')
+@section('content');
     <!-- /.header start -->
     <style>
         .pagination > li > a,
@@ -18,152 +18,112 @@
             color: #000 !important;
         }
     </style>
-    <header class="header fixed-top">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="/home"><img src="{{ URL::asset('images/logo/nav_logo.png') }}"  alt="N_Studio_Zumba_Logo"></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link " href="/home">
-                                home<span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/index/about">
-                                about<span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/admin/dashboard/admin_gallery">
-                                gallery<span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/index/class_packages">
-                                classes<span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/index/schedule">
-                                schedule<span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/index/testimonials">
-                                testimonials<span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/index/contact">contact</a>
-                        </li>
 
-                        {{--User name and logout button start--}}
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->username }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                        {{--User name and logout button end--}}
-
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <!-- /.header start -->
+    @extends('layouts.hori_sidebar');
     <!--header end-->
 
-    <br><br><br><br>
-    @if (session('msgupdt'))
-        <div class="alert alert-success" role="alert">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            {{ session('msgupdt') }}
-        </div>
-    @endif
-<div class="panel panel-default">
-    <div class="panel-body">
-        <table class="table thread-dark" width="100%" >
-            <thead>
-            <tr>
-                <th width="250">Title</th>
-                <th width="700">Post</th>
-                <th width="100">Created at</th>
-                <th></th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($posts as $post)
-                <tr>
-                    <td>{{ $post->title }}</td>
-                    <td>{{ $post->post_body }}</td>
-                    <td>{{ $post->updated_at }}</td>
-                    <td>
-                        <div class="row">
-                            <div class="col">
-                                <a href="{{url('admin/create_notifications/'.$post->id.'/update')}}"><button class="editbtn" >UPDATE</button></a>
-                            </div>
+    <!--Show posts area start-->
+    <div class="about-area pad90">
+        <div class="container-fluid">
+            <div class="row mb-0">
+                <!--Sidebar-area start-->
+                @extends('layouts.vertical_sidebar');
+                <!--Sidebar-area end-->
+
+                <div class="card col-lg-10 col-md-9 mainFix col-lg-offset-2 col-md-offset-3 overview-block pad30 rounded" style="margin-left: 250px;">
+
+                    @if (session('msgupdt'))
+                        <div class="alert alert-success fs-15" role="alert">
+                            <button type="button" class="close" data-dismiss="alert">×</button>
+                            {{ session('msgupdt') }}
                         </div>
-                    </td>
-                    <td>
-                        <button type="button" class="delbtn" data-toggle="modal" data-target="#myModal-{{ $post->id }}">
-                            DELETE
-                        </button>
-                        <!--modal-->
-                        <div class="modal fade" id="myModal-{{ $post->id }}">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <!-- Modal Header -->
-                                    <div class="modal-header">
-                                        <h4 class="modal-title" style="color: black">Delete Post</h4>
-                                        <button type="button" class="close" data-dismiss="modal">×</button>
-                                    </div>
+                    @endif
 
-                                    <!-- Modal body -->
-                                    <div class="modal-body" style="color: black">
-                                        <b>Are you sure you want to delete this post?</b>
-                                    </div>
+                    <div class="section-title text-center">
+                        <div class="title-bar full-width mb20">
+                            <img src="{{ URL::asset('images/logo/ttl-bar.png') }}" alt="title-img">
+                        </div>
+                        <h3>View Posts</h3>
+                    </div>
 
-                                    <!-- Modal footer -->
+                    <!--show posts area start-->
+                    <div class="pricing-area text-center pad30 col-md-10 px-4 ml-4">
+                        <div class="container">
+                            <div class="panel panel-default ml90">
+                                <div class="panel-body">
+                                    <table class="table table-striped table-hover" width="100%" >
+                                        <thead>
+                                            <tr>
+                                                <th width="250">Title</th>
+                                                <th width="600">Post</th>
+                                                <th width="200">Created at</th>
+                                                <th></th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach($posts as $post)
+                                                <tr>
+                                                    <td>{{ $post->title }}</td>
+                                                    <td>{{ $post->post_body }}</td>
+                                                    <td>{{ $post->updated_at }}</td>
+                                                    <td>
+                                                        <div class="row">
+                                                            <div class="col">
+                                                                <a href="{{url('admin/create_notifications/'.$post->id.'/update')}}"><button class="editbtn" >UPDATE</button></a>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <button type="button" class="delbtn" data-toggle="modal" data-target="#myModal-{{ $post->id }}">DELETE</button>
+                                                        <!--modal-->
+                                                        <div class="modal fade" id="myModal-{{ $post->id }}">
+                                                            <div class="modal-dialog">
+                                                                <div class="modal-content">
+                                                                    <!-- Modal Header -->
+                                                                    <div class="modal-header">
+                                                                        <h4 class="modal-title" style="color: black">Delete Post</h4>
+                                                                        <button type="button" class="close" data-dismiss="modal">×</button>
+                                                                    </div>
 
-                                    <div class="modal-footer">
-                                        <form method="POST" action="{{action('PostController@destroy',$post->id)}}">
-                                            @csrf
-                                            {{ method_field('DELETE') }}
-                                            <button type="submit" class="delbtn">Yes</button>
-                                        </form>
-                                        <button type="button" class="delbtn" data-dismiss="modal">No</button>
-                                    </div>
+                                                                    <!-- Modal body -->
+                                                                    <div class="modal-body" style="color: black">
+                                                                        <b>Are you sure you want to delete this post?</b>
+                                                                    </div>
+
+                                                                    <!-- Modal footer -->
+
+                                                                    <div class="modal-footer">
+                                                                        <div class="row">
+                                                                            <form method="POST" action="{{action('PostController@destroy',$post->id)}}">
+                                                                                @csrf
+                                                                                {{ method_field('DELETE') }}
+                                                                                <button type="submit" class="delbtn mr-1">Yes</button>
+                                                                            </form>
+                                                                            <button type="button" class="delbtn ml-1" data-dismiss="modal">No</button>
+                                                                        </div>
+                                                                    </div>
+
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                    {!! $posts->links(); !!}
 
                                 </div>
                             </div>
+
                         </div>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-
-        {!! $posts->links(); !!}
-
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-<br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <!--Show posts area end-->
+    <br><br><br><br><br><br><br><br><br><br><br><br><br>
 @endsection
